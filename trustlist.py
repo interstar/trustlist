c04ebbec185420d476c709cb330dbf590eb7bfa3
Eli <egothill@gmail.com>
2011-11-21 22:13:21 +0000

    Initial commit

---
+++ trustlist.py
@@ -0,0 +1,75 @@
+# import tweepy
+# import auth
+# import MySQLdb
+# from datetime import datetime
+# from dateutil.relativedelta import *
+# from settings import *
+# import sys
+# import warnings
+# import logging, logging.handlers
+# import string
+
+        
+# SETTINGS
+        
+import tweepy
+
+CONSUMER_KEY = 'TNLBafeEFj0ldrmJYHz2A'
+CONSUMER_SECRET = '4h12ZbAkTLkI2oMwGn7e77dlOmX5anf5EnBNHLtEBk'
+
+ACCESS_KEY = '17024753-TUzJNjFMXSsMuzNBxLL4y22nL5YsIs8J1uFZ9Ts8i'
+ACCESS_SECRET = '6nM1eWdxgmQzbYmmDyGy2PUcjl8JeAq1upzNgiCxk'
+
+auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
+auth.set_access_token(ACCESS_KEY, ACCESS_SECRET)
+api = tweepy.API(auth)
+
+# seed_user = 'webisteme'
+# seed_list =    'punkmoney-trusted'    
+        
+# BUILD TRUSTNET
+
+trust_list = []
+new_list = []
+
+def buildList(seed_user, list_name):
+
+    users = api.list_members(seed_user,list_name)[0]
+    
+    for user in users:
+        trust_list.append(user.screen_name.lower())
+
+    # crawl deeper    
+    
+    new_list = crawlDeeper(trust_list, list_name)
+    while len(new_list) > 0 : new_list = crawlDeeper(new_list, list_name)
+        
+    # update database
+    
+    return trust_list
+        
+        
+# CRAWL DEEPER (only call from buildList())
+
+def crawlDeeper(list, list_name):
+    new_list[:] = []
+    for user in list:
+        print 'checking %s' % user
+        user = user.lower()
+        try:
+            candidates = api.list_members(user,list_name)[0]
+            for candidate in candidates:
+                print '--checking candidate %s isn\'t already in trust list' % candidate.screen_name
+                try:
+                    trust_list.index(candidate.screen_name.lower())
+                except:
+                    print '--adding user %s to trust list' % candidate.screen_name.lower()
+                    trust_list.append(candidate.screen_name.lower())
+                    new_list.append(candidate.screen_name.lower())
+        except:
+            continue
+    return new_list
+    
+
+print buildList('webisteme', 'punkmoney-trusted')
+        
