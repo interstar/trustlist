0d328637aa80a1f28fc674519ecb71c2cd7eea5d
Eli <egothill@gmail.com>
2011-11-22 19:57:52 +0000

    Initial commit

---
+++ settings.py
@@ -0,0 +1,17 @@
+''' Twitter API credentials '''
+
+CONSUMER_KEY = 'WWmfSbD0Mzod7W2wMcxhrQ'
+CONSUMER_SECRET = '0sYSFp9hsiUJRLR5xR9Pcx5yRhj8MLTLHLQDL894gy8'
+
+ACCESS_KEY = '415040376-jzyCB4EIMra25UDbXrJkUWoxrfWOEbE19z9ahWl7'
+ACCESS_SECRET = 'ftMAdFzVcZ2Ym2yXVkuaUZ2V6iTfoxgJVQBtguczro'
+
+''' Seed list and user '''
+
+# Name of the user who owns the seed list, the list to start the crawl from. (Leave out the '@')
+
+seed_user = 'webisteme'
+
+# Name of the list to crawl
+
+list_name = 'punkmoney-trusted'
